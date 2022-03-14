8426cf11b261944f7b09a3bc82b47449cdf3cc17
Pratik Devle <pratik.devle98@gmail.com>
2022-03-14 16:44:55 +0530

    initial commit

---
+++ mockbee-ecom-test/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import logo from "./logo.png";
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+function App() {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get("/api/products");
+      setProducts(response.data.products);
+    })()
+  }, [])
+  return (
+    <div className="App">
+      <h1>Mockbee Test</h1>
+      {products.map(item => (
+        <div key={item.id}>
+          <h1>{item.id}</h1>
+        <h2>{item.title}</h2>
+        <h3>{item.author}</h3>
+        <h4>{item.price}</h4>
+        </div>
+        
+      ))}
+    </div>
+  );
+}
+
+export default App;
